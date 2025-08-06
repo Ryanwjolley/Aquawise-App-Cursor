054dc52738b639c9ef17f06e8804f7e9cbbb9c46
Ryan Jolley <ryanj@jonesanddemille.com>
2025-08-06 23:26:03 +0000

    Initial commit of prototype from Project IDX

---
+++ functions/lib/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// This file is intentionally left blank. 
+// The password reset logic has been moved to a Server Action in `src/lib/actions.ts`
+// and is triggered by the UI at `/admin/reset-password`.
+// This avoids the complexity of deploying and calling a separate Cloud Function for this one-time task.
+//# sourceMappingURL=index.js.map
